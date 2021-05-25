8ddb99d1197e90eb246b04c4013b5e031db1a29c
jhon <jveramdaw1@gmail.com>
2021-05-25 20:57:20 +0200

    Creado caso prueba

---
+++ CalcularPeriodo/src/CalcularPeriodoTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalcularPeriodoTest {
+
+	@Test
+	void testCalcularPeriodo() {
+		fail("Not yet implemented");
+		CalcularPeriodo c = new CalcularPeriodo();
+		assertNotNull(c);
+	}
+
+}
